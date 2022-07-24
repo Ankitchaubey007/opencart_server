4a05017ec0b200e74abce9f109aafac7a0fc4375
Ankit kashyap <webdev0077@gmail.com>
2022-07-24 23:50:47 +0530

    complete category apis

--- index.js
+++ index.js
@@ -203,36 +203,176 @@ app.put("/update_staff/:id", (req, res) => {
     }
 });
 
-app.put("/update_staff_password/:id",async (req,res) =>{
-    const token =req.headers['token'];
-    const decoded = await verify_token (token,secret,res);
+app.put("/update_staff_password/:id", async (req, res) => {
+    const token = req.headers['token'];
+    const decoded = await verify_token(token, secret, res);
     if (decoded && decoded.role == 'staff' && decoded.id == req.params.id) {
         const id = req.params.id;
         const old_password = req.body.old_password;
         const new_password = req.body.new_password;
         const confirm_password = req.body.confirm_password;
-        if( new_password == confirm_password){ 
+        if (new_password == confirm_password) {
             const update_staff_query = `UPDATE admin SET password =? WHERE admin_id =? AND password = ?`;
-            connection.query (update_staff_query,[new_password, id, old_password], (err,data) => {
-                 if(err){
+            connection.query(update_staff_query, [new_password, id, old_password], (err, data) => {
+                if (err) {
                     res.send({
                         seccess: false,
-                        error:err.sqlMessage,
+                        error: err.sqlMessage,
                         data: []
                     })
-                 } else{ 
-                    res.send ({
+                } else {
+                    res.send({
                         success: true,
                         error: "",
                         data: "staff updated"
                     });
-                 }
+                }
             });
         }
     }
 });
 
 //--------------------admin api-------------------------------
+
+
+//--------------------category apis---------------------------
+app.post('/add_category', (req, res) => {
+    const token = req.headers['token'];
+    let decoded = undefined;
+    try {
+        decoded = jwt.verify(token, secret);
+    } catch (err) {
+        res.status(401).send({
+            success: false,
+            msg: "Invalid Token",
+            data: [],
+            length: 0
+        });
+    }
+    if (decoded.role == 'admin' || decoded.role == 'staff') {
+        const category_name = req.body.category_name;
+        const add_category_query = `INSERT INTO category (category_name) VALUES (?)`;
+        connection.query(add_category_query, [category_name], (err, data) => {
+            if (err) {
+                res.send({
+                    success: false,
+                    error: err.sqlMessage,
+                    data: []
+                });
+            } else {
+                res.send({
+                    success: true,
+                    error: "",
+                    data: "category added"
+                });
+            }
+        });
+    }
+});
+
+app.put("/update_category/:id", (req, res) => {
+    const token = req.headers['token'];
+    if (token) {
+        let decoded = undefined;
+        try {
+            decoded = jwt.verify(token, secret);
+        } catch (err) {
+            res.status(401).send({
+                success: false,
+                msg: "Invalid Token",
+                data: [],
+                length: 0
+            });
+        }
+        if (decoded.role == 'admin' || decoded.role == 'staff') {
+            const id = req.params.id;
+            const category_name = req.body.category_name;
+            const update_category_query = `UPDATE category SET category_name = ? WHERE category_id = ?`;
+            connection.query(update_category_query, [category_name, id], (err, data) => {
+                if (err) {
+                    res.send({
+                        success: false,
+                        error: err.sqlMessage,
+                        data: []
+                    });
+                } else {
+                    res.send({
+                        success: true,
+                        error: "",
+                        data: "category updated"
+                    });
+                }
+            });
+        } else {
+            res.status(401).send({
+                success: false,
+                msg: "you are not authorized to perform this action",
+                data: [],
+                length: 0
+            });
+        }
+    } else {
+        res.send({
+            success: false,
+            error: "Invalid Token",
+            data: []
+        });
+    }
+});
+app.put("/update_category_status/:id/:status", (req, res) => {
+    const token = req.headers['token'];
+    if (token) {
+        let decoded = undefined;
+        try {
+            decoded = jwt.verify(token, secret);
+        } catch (err) {
+            res.status(401).send({
+                success: false,
+                msg: "Invalid Token",
+                data: [],
+                length: 0
+            });
+        }
+        if (decoded.role == 'admin' || decoded.role == 'staff') {
+            const id = req.params.status;
+            const update_category_query = `UPDATE category SET is_active = ? WHERE category_id = ?`;
+            connection.query(update_category_query, [status, id], (err, data) => {
+            if (err) {
+                res.send({
+                    success: false,
+                    error: err.sqlMessage,
+                    data: []
+                });
+            }else {
+                res.send({
+                    success: true,
+                    error: "",
+                    data: "category updated"
+                });
+            }
+            });
+        }else {
+            res.status (401).send ({
+                success: false,
+                msg: "you are not authorized to perform this action",
+                data: [],
+                length: 0
+            });
+        }
+    }else{
+        res.send({
+            success: false,
+            error: "Invalid Token",
+            data: []
+        });
+    }
+});
+
+//--------------------category apis END-----------------------
+
+
+//--------------------product apis----------------------------
+
 
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
